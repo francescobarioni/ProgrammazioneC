3e083d2ac0dff89b4400042766e945a67c0275b3
francescobarioni <73527470+francescobarioni@users.noreply.github.com>
2020-10-28 22:58:00 +0100

    Add files via upload

--- FattorialeChallenge.cpp
+++ FattorialeChallenge.cpp
@@ -2,52 +2,22 @@
 Come gestire valori enormi come 1000?!*/
 
 #include <stdio.h>
-#include <math.h>
 
 main() {
-	int numero, fattoriale = 1, n, f;
-	int i,cifre = 0;
-	int cifra;
-	int cont = 0;
-	int j;
+	int numero;
 	
 	printf("Inserisci il numero desiderato: \n");
 	scanf("%d", &numero);
 	
-	n = numero;
-	
 	if(numero == 0){// controllo se il numero inserito è 0 (caso particolare del fattoriale)
 		printf("Il fattoriale di %d è 1 e il numero di zeri nel fattoriale e' 0.\n", numero);
 	} else 
-	{
-		//calcolo fattoriale
-		for(i = numero; i > 1; i--){
-			fattoriale = fattoriale * i;
+	{   	 
+		int somma = 0;       					
+        while(numero > 5){
+   			numero = numero / 5;
+   			somma = somma + numero;
 		}
-		printf("Il fattoriale del numero inserito e' %d.\n", fattoriale);
-		f = fattoriale;
-		
-		//calcolo del numero di cifre
-		while(fattoriale > 0){
-        	fattoriale = fattoriale / 10;
-        	cifre++;
-    	}
-    	printf("Il fattoriale ha %d cifre.\n", cifre);
-    	
-    	fattoriale = f;
-        	        
-        	
-        	for(j = 0; j < cifre; j++){
-        		cifra = fattoriale % 10;        		
-				 
-            		if(0 == cifra){
-            			cont++;//conta il numero di 0
-					} else {
-						break;
-					}  
-					fattoriale = fattoriale / 10;         
-        	}
-   		printf("Il numero di zeri presenti nel fattoriale e' %d.\n", cont);
-
+   		printf("Il numero di zeri presenti nel fattoriale e' %d.\n", somma);
 	}
 }
